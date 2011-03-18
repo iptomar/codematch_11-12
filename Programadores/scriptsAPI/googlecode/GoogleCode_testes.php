a4799f63191e5f48ad41ef2fb1a8bda7bd606d76
Bruno Macao <bruno.macao@gmail.com>
2011-03-18 19:56:08 +0000

    pesquisa de projectos no Google Code (detalhes do script dentro do mesmo)

---
+++ Programadores/scriptsAPI/googlecode/GoogleCode_testes.php
@@ -0,0 +1,18 @@
+<?php
+
+//Pesquisa de projectos no Google Code por linguagem de programação
+//Usando o url abaixo definido conseguimos listar os projectos existentes nesse server
+//A pesquisa é definida pelo parametro 'label:' seguido da linguagem de programação
+//Devolve uma listagem de pesquisa como o Google normal indexada e paginada.
+
+$url = "http://code.google.com/hosting/search?q=label:Java";
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);  
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    $body = curl_exec($ch);
+    curl_close($ch);
+    print_r($body);
+
+?>
+
+//link para a API/Json - https://ajax.googleapis.com/ajax/services/search/web

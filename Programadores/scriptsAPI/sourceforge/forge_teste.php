23aa2f28435de831d95cc85ee0125b901eac415d
Coquim <filipecoquim@gmail.com>
2011-03-19 14:55:25 +0000

    Lista os projectos activos no sourceforge

---
+++ Programadores/scriptsAPI/sourceforge/forge_teste.php
@@ -0,0 +1,35 @@
+<?php
+	//lista os  projectos do sourceforge activos, do 1 projecto a um numero detrminado por nós...
+	//o ideial era apanhar o numero do ultimo projecto no ssourceforge criado para ser um numero certo, não um numero aleatorio
+	//o exemplo abaixo lista os projecto activos dos primeiros 800 projectos criados
+	$aux=0;//variavel para mostrar quantos projectos estão activos
+	$total=800;
+	$pp ="programming-languages";
+	for($teste= 1;$teste<=$total;$teste++)
+	{
+	 	if( !ini_get('safe_mode') )
+		{
+            set_time_limit(180);
+        }
+		$ch = @curl_init();
+		$url="http://sourceforge.net/api/project/id/$teste/json";
+		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Googlebot/2.1');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$page = curl_exec( $ch);
+		curl_close($ch);
+		//descodifica uma string json
+		$json = json_decode($page);	
+			//se nao exitir projecto nao apresenta
+			if($json->Project->id != null)			
+				{
+				//apresenta alguns dados do projecto
+					print_r("<b>ID: </b>" .($json->Project->id)."<br>");
+					print_r("<b>Nome: </b>" .($json->Project->name)."<br>");
+					print_r("<b>Ranking: </b>" .($json->Project->ranking)."<br>");
+					print_r("<b>Languages: </b>" .($json->Project->$pp)."<br><br>");
+					$aux++;
+				}
+	}
+	echo "Total de projectos activos: " .$aux, " em " .$total;
+?>

d33e88ee4e797877fd0776280ddea8759e791043
g-esteves <goncalo.e@gmail.com>
2011-03-29 16:43:08 +0100

    Alterações Script Bing API: - alterado estrutura do código - adicionado restantes "search's"

--- Programadores/scriptsSearch/Bing/bing_teste.php
+++ Programadores/scriptsSearch/Bing/bing_teste.php
@@ -4,50 +4,94 @@ if ($_GET["word"]) {
 }
 //#################################################################################################################################
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if ((!empty($_POST['word'])) || ($_POST['radiobt']=='github') || ($_POST['radiobt']=='sourceforge') || ($_POST['radiobt']=='launchpad') || ($_POST['radiobt']=='bitbucket') || ($_POST['radiobt']=='google') || ($_POST['radiobt']=='githuball')) {
-		//radio button gihtub activado
-		if ($_POST['radiobt']=='github') {
-			$word = '"http://github.com/"site:github.com'; //palavra a pesquisar
-		}
-		else if ($_POST['radiobt']=='sourceforge') {
-			$word = '"http://sourceforge.net/projects/"site:sourceforge.net';  //palavra a pesquisar
-		}
-		else if ($_POST['radiobt']=='launchpad') {
-			$word = '"http://launchpad.net/"site:launchpad.net';  //palavra a pesquisar
-		}
-		else if ($_POST['radiobt']=='bitbucket') {
-			$word = '"http://bitbucket.org/"site:bitbucket.org';  //palavra a pesquisar
-		}
-		else if ($_POST['radiobt']=='google') {
-			$word = '"http://code.google.com/p/"site:code.google.com';  //palavra a pesquisar
-		}
-		else {
-			$word = trim($_POST['word']); //palavra a pesquisar
-		}
+	if ((empty($_POST['word'])) && ($_POST['radiobt']!='array')) {
 		$lenght = trim($_POST['tamanho']); //quantas pesquisas
 		//controla o valor de pesquisas inserido
 		if (($lenght < 0) || ($lenght > 50) || (empty($lenght))) {
 			$lenght = 10;
 		}
+		//radio button gihtub activado
+		if ($_POST['radiobt']=='github') {
+			$word = '"http://github.com/"site:github.com'; //palavra a pesquisar
+			$resultados_a_mostrar = search($word,$lenght);
+		}
+		//radio button sourceforge activado
+		else if ($_POST['radiobt']=='sourceforge') {
+			$word = '"http://sourceforge.net/projects/"site:sourceforge.net';  //palavra a pesquisar
+			$resultados_a_mostrar = search($word,$lenght);
+		}
+		//radio button launchpad activado
+		else if ($_POST['radiobt']=='launchpad') {
+			$word = '"http://launchpad.net/"site:launchpad.net';  //palavra a pesquisar
+			$resultados_a_mostrar = search($word,$lenght);
+		}
+		//radio button bitbucket activado
+		else if ($_POST['radiobt']=='bitbucket') {
+			$word = '"http://bitbucket.org/"site:bitbucket.org';  //palavra a pesquisar
+			$resultados_a_mostrar = search($word,$lenght);
+		}
+		//radio button google activado
+		else if ($_POST['radiobt']=='google') {
+			$word = '"http://code.google.com/p/"site:code.google.com';  //palavra a pesquisar
+			$resultados_a_mostrar = search($word,$lenght);
+		}
 		//radio button activado do githuball
-		if ($_POST['radiobt']=='githuball') {
+		else if ($_POST['radiobt']=='githuball') {
 			$word = '"http://github.com/"site:github.com';  //palavra a pesquisar
-			//pagina do api
-			$request ='http://api.search.live.net/json.aspx?Appid=83019BDA3590E9CC61CBB51C2385A72F93810B47&Query='.$word.'&Sources=Web&Web.Count=1';
-			//recebe as pagina
-			$response = file_get_contents($request);
-			//descodifica a pagina
-			$jsonobj = json_decode($response);
-			$total_resultados = $jsonobj->SearchResponse->Web->Total;
 			$i=0;
-			$dados = "";
-			while ($i <= $total_resultados) {			
-				$dados .= searchfull($word,$lenght,$i);
+			file_put_contents("dados/dadosgit.txt", "");
+			while ($i <= 1000) {
+				file_put_contents("dados/dadosgit.txt", searchfull($word,$lenght,$i), FILE_APPEND);
 				$i=$i+$lenght;
 			}
-			file_put_contents("dados/dados.txt", $dados);
-			$resultados_a_mostrar = "Concluido<br><a href='dados/dados.txt'>Link ficheiro txt com os resultados!</a>";		
-		} else {
+			$resultados_a_mostrar = "Concluido<br><a href='dados/dadosgit.txt'>Link ficheiro txt com os resultados!</a>";
+		}
+		//radio button activado do sourceforgeall
+		else if ($_POST['radiobt']=='sourceforgeall') {
+			$word = '"http://sourceforge.net/projects/"site:sourceforge.net';  //palavra a pesquisar
+			$i=0;
+			file_put_contents("dados/dadossource.txt", "");
+			while ($i <= 1000) {
+				file_put_contents("dados/dadossource.txt", searchfull($word,$lenght,$i), FILE_APPEND);
+				$i=$i+$lenght;
+			}
+			$resultados_a_mostrar = "Concluido<br><a href='dados/dadossource.txt'>Link ficheiro txt com os resultados!</a>";
+		}
+		//radio button activado do launchpadall
+		else if ($_POST['radiobt']=='launchpadall') {
+			$word = '"http://launchpad.net/"site:launchpad.net';  //palavra a pesquisar
+			$i=0;
+			file_put_contents("dados/dadoslaunchpad.txt", "");
+			while ($i <= 1000) {
+				file_put_contents("dados/dadoslaunchpad.txt", searchfull($word,$lenght,$i), FILE_APPEND);
+				$i=$i+$lenght;
+			}
+			$resultados_a_mostrar = "Concluido<br><a href='dados/dadoslaunchpad.txt'>Link ficheiro txt com os resultados!</a>";
+		}
+		//radio button activado do bitbucketall
+		else if ($_POST['radiobt']=='bitbucketall') {
+			$word = '"http://bitbucket.org/"site:bitbucket.org';  //palavra a pesquisar
+			$i=0;
+			file_put_contents("dados/dadosbitbucket.txt", "");
+			while ($i <= 1000) {
+				file_put_contents("dados/dadosbitbucket.txt", searchfull($word,$lenght,$i), FILE_APPEND);
+				$i=$i+$lenght;
+			}
+			$resultados_a_mostrar = "Concluido<br><a href='dados/dadosbitbucket.txt'>Link ficheiro txt com os resultados!</a>";
+		}
+		//radio button activado do googleall
+		else if ($_POST['radiobt']=='googleall') {
+			$word = '"http://code.google.com/p/"site:code.google.com';  //palavra a pesquisar
+			$i=0;
+			file_put_contents("dados/dadosgoogle.txt", "");
+			while ($i <= 1000) {
+				file_put_contents("dados/dadosgoogle.txt", searchfull($word,$lenght,$i), FILE_APPEND);
+				$i=$i+$lenght;
+			}
+			$resultados_a_mostrar = "Concluido<br><a href='dados/dadosgoogle.txt'>Link ficheiro txt com os resultados!</a>";
+		}
+		else {
+			$word = trim($_POST['word']); //palavra a pesquisar
 			$resultados_a_mostrar = search($word,$lenght);
 		}
 	} else {
@@ -130,11 +174,15 @@ function searchfull($word,$lenght,$offset=0) {
 		</div>
 		<div style="position: relative;float: right;width: 60%; ">
 			<input type="radio" name="radiobt" value="github"> Github search</input><br><br>
-			<input type="radio" name="radiobt" value="githuball"> Github search ALL - pode demorar muito tempo!</input><br><br>
+			<input type="radio" name="radiobt" value="githuball"> Github search ALL - pode demorar algum tempo!</input><br><br>
 			<input type="radio" name="radiobt" value="sourceforge"> Sourceforge search</input><br><br>
+			<input type="radio" name="radiobt" value="sourceforgeall"> Sourceforge search ALL - pode demorar algum tempo!</input><br><br>
 			<input type="radio" name="radiobt" value="launchpad"> Launchpad search</input><br><br>
+			<input type="radio" name="radiobt" value="launchpadall"> Launchpad search ALL - pode demorar algum tempo!</input><br><br>
 			<input type="radio" name="radiobt" value="bitbucket"> Bitbucket search</input><br><br>
+			<input type="radio" name="radiobt" value="bitbucketall"> Bitbucket search ALL - pode demorar algum tempo!</input><br><br>
 			<input type="radio" name="radiobt" value="google"> Google Code search</input><br><br>
+			<input type="radio" name="radiobt" value="googleall"> Google search ALL - pode demorar algum tempo!</input><br><br>
 			<input type="radio" name="radiobt" value="array"> Mostrar pesquisas num array</input><br><br>
 		</div>
 	</form>

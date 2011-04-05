9718ff43546d132e4dda859d0c054972b59b3947
aGonc <ag_gui99@hotmail.com>
2011-04-05 21:55:54 +0100

    novas aplicações

--- Programadores/scriptsSearch/Yahoo/yahoo_test.php
+++ Programadores/scriptsSearch/Yahoo/yahoo_test.php
@@ -21,7 +21,7 @@ $query = urlencode("php");
 
 $pages ="&start=0&count=20";
 
-
+echo"###############GITHUB##################";
 $fullURL = $YahooSearchAPI . $query . $UrlAppid . $format . $SiteGH . $pages;
 
 $session = curl_init($fullURL);
@@ -36,7 +36,98 @@ curl_close($session);
 
 $results = json_decode($resultsJson);
 
-foreach ($results->{'ysearchresponse'}->{'resultset_web'} as $result) 
+echo "<pre>";
+//mostra array
+print_r($results);
+echo "</pre>";
+echo "<br />";
+
+
+echo"###############SOURCEFORGE##################";
+$fullURL = $YahooSearchAPI . $query . $UrlAppid . $format . $SiteSF . $pages;
+
+$session = curl_init($fullURL);
+
+curl_setopt($session, CURLOPT_HEADER, false); 
+
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true); 
+
+$resultsJson = curl_exec($session); 
+
+curl_close($session);
+
+$results = json_decode($resultsJson);
+
+echo "<pre>";
+//mostra array
+print_r($results);
+echo "</pre>";
+echo "<br />";
+
+echo"###############BITBUCKET##################";
+$fullURL = $YahooSearchAPI . $query . $UrlAppid . $format . $SiteBB . $pages;
+
+$session = curl_init($fullURL);
+
+curl_setopt($session, CURLOPT_HEADER, false); 
+
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true); 
+
+$resultsJson = curl_exec($session); 
+
+curl_close($session);
+
+$results = json_decode($resultsJson);
+
+echo "<pre>";
+//mostra array
+print_r($results);
+echo "</pre>";
+echo "<br />";
+
+echo"###############LAUNCHPAD##################";
+$fullURL = $YahooSearchAPI . $query . $UrlAppid . $format . $SiteLP . $pages;
+
+$session = curl_init($fullURL);
+
+curl_setopt($session, CURLOPT_HEADER, false); 
+
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true); 
+
+$resultsJson = curl_exec($session); 
+
+curl_close($session);
+
+$results = json_decode($resultsJson);
+
+echo "<pre>";
+//mostra array
+print_r($results);
+echo "</pre>";
+echo "<br />";
+
+echo"###############GOOGLE##################";
+$fullURL = $YahooSearchAPI . $query . $UrlAppid . $format . $SiteGG . $pages;
+
+$session = curl_init($fullURL);
+
+curl_setopt($session, CURLOPT_HEADER, false); 
+
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true); 
+
+$resultsJson = curl_exec($session); 
+
+curl_close($session);
+
+$results = json_decode($resultsJson);
+
+echo "<pre>";
+//mostra array
+print_r($results);
+echo "</pre>";
+echo "<br />";
+
+/*foreach ($results->{'ysearchresponse'}->{'resultset_web'} as $result) 
 
 {
 
@@ -46,6 +137,6 @@ echo "<p>{$result->{'abstract'}}</p>\n";
 
 }
 
-	
+*/	
 	
 ?>

df73c6741b0210f058ebe733c803b45e11323739
Joao Cardoso <se_mandas_isso_morres@hotmail.com>
2011-04-05 15:42:26 +0100

    Controlo de aplicações em perl

--- Programadores/Dispatcher/teste_waitpid.pl
+++ Programadores/Dispatcher/teste_waitpid.pl
@@ -2,16 +2,16 @@
 
 use IPC::Open3;
 
-  $cmd = "cd c:\dados "; #comando a efectuar
-   
-  my $pid = open3(\*WRITER, \*READER, \*ERROR, $cmd);
+my ($wrt, $rdr, $err);
+my $cmd = "cd c:\Perl64 "; #comando a efectuar
+my $pid = open3($wrt, $rdr, $err, $cmd);
 
- 	waitpid( $pid, 0 ) or die "$!\n";
+waitpid( $pid, 0 ) or die "$!\n"; #esperar que o processo acabe
 
-	my $ret = $?;
+my $ret = $?; #guarda o estado final
 
-	if($ret != 0){
-		print ("\nOcorreu um erro, verifique os registos, retorno:".$ret.".\n");    #se ocorre um erro devolve o código do erro
-	} else {
-		print ("Sucesso!!\n"); #senão devolve mensagem de sucesso
-	}
+if($ret != 0){
+	print ("\nOcorreu um erro, verifique os registos, retorno:".$ret.".\n");    #se ocorre um erro devolve o código do erro
+} else {
+	print ("Sucesso!!\n"); #senão devolve mensagem de sucesso
+}

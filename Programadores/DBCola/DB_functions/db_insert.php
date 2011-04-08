ab41deb144465461dd2457696b232dd7612fb4b7
Bruno Macao <bruno.macao@gmail.com>
2011-04-08 13:47:02 +0100

    Function to insert on DB using the DBcola schema

--- Programadores/DBCola/DB_functions/db_insert.php
+++ Programadores/DBCola/DB_functions/db_insert.php
@@ -1,13 +1,43 @@
 <?php
-//inserting data
-function db_insert($project,$source,$repository,$author,$language,$last_up,$logo)
+
+//This function inserts/update data on DB and returns the function stats due to sucess or fail of the operation
+// Usage:
+// include 'db_insert'; -> API search script file
+// db_insert(<arguments>); 7 arguments accepted - if some are inexistent use NULL value to maintain DB structure
+
+function insert_db($Name,$Source,$Repo,$Author,$Lang,$LsUp,$Logo){
+try{
 require_once('phpcassa/connection.php');
 require_once('phpcassa/columnfamily.php');
+
 //make connection with database
 $conn = new Connection('tolmai');
+
 //select column family
-$column_family= new ColumnFamily($conn , 'tm');
+$column_family= new ColumnFamily($conn , 'Project');
+
 //insert column family rows
-$column_family->insert('tm',array('tm_nameproj' =>$project,'tm_source' =>$source,'tm_repository' =>$repository,'tm_author'=>$author,'tm_language'=>$language,'tm_lastupdate'=>$last_up,'tm_logo'=>$logo));
+//currently Database Proposed Schema
+//Index column -> Project - Project Name (index key)
+//Single columns ->tm_source - project url; tm_repository - repository url ; tm_author - projects author; tm_language - project languages [SuperColumn]; tm_lastupdate - projects last update; tm_logo - projects logo url
+$column_family->insert('$Name',array('tm_source' =>$Source,'tm_repository' =>$Repo, 'tm_author' =>$Author, 'tm_language' =>$Lang, 'tm_lastupdate' =>$LsUp, 'tm_logo' =>$Logo));
+$status ="Sucess to insert / update DB";
+return $status;
+
+}catch(Exception x){
+	$status = "Fail to insert / update DB" ;
+	return $status;
+}
+}
+//Json Representation
+//  {
+//  "$Name" : {
+//    "tm_source" : "Value",
+//    "tm_repository" : "Value",
+//    "tm_author" : "Value",
+//    "tm_language" : "Value",
+//    "tm_lastupdate" : "Value",
+//    "tm_logo" : "Value"
+//  }
 
 ?>

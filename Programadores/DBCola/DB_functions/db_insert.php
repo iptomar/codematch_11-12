4998ec54a3bd5a7432320770bba260a154967c85
Bruno Macao <bruno.macao@gmail.com>
2011-04-12 21:43:11 +0100

    update

--- Programadores/DBCola/DB_functions/db_insert.php
+++ Programadores/DBCola/DB_functions/db_insert.php
@@ -5,7 +5,7 @@
 // include 'db_insert.php'; -> on API search script file
 // db_insert(<arguments>); 10 arguments accepted - if some are inexistent use Zero value to maintain DB structure
 
-function insert_db($Name,$Fullname,$Source,$Repo,$Author,$Lang_P,$Lang_S,$Lang_T,$LsUp,$Logo){
+function insert_db($Name,$Fullname='',$Source,$Repo,$Author,$Lang_,$LsUp,$Logo=''){
 try{
 require_once('phpcassa/connection.php');
 require_once('phpcassa/columnfamily.php');
@@ -16,12 +16,21 @@ $conn = new Connection('tolmai');
 //select column family
 $column_family= new ColumnFamily($conn , 'tm');
 
+$comp = count($Author);
+
 //insert column family rows
 //currently Database Proposed Schema
 //Index column -> Name - Project Name (index key) (ex: "mozilla" stays "mozilla firefox 4")
-//Single columns ->tm_fullname - fullname; tm_source - project url; tm_repository - repository url ; tm_author - projects author; tm_language - project languages [SuperColumn]; tm_lastupdate - projects last update; tm_logo - projects logo url
+//Single columns ->tm_fullname - fullname; tm_source - project url; tm_repository - repository url ;
+// tm_author [Array]- projects author; tm_language [Array] - project languages ; tm_lastupdate - projects last update; tm_logo - projects logo url
 //SuperColumn tm_language -> To decide how many entries will take, this function draft takes 3 entries ordered by project relevance
-$column_family->insert($Name,array('tm_fullname' =>$Fullname,'tm_source' =>$Source,'tm_repository' =>$Repo, 'tm_author' =>$Author, 'tm_language' =>array('tm_Main' => $Lang_p, 'tm_Secundary' => $Lang_S, 'tm_terciary' => $Lang_T), 'tm_lastupdate' =>$LsUp, 'tm_logo' =>$Logo));
+$column_family->insert($Name,array('tm_fullname' =>$Fullname,'tm_source' =>$Source,'tm_repository' =>$Repo, 'tm_language' =>array('tm_Main' => $Lang_[0], 'tm_Secundary' => $Lang_[1], 'tm_terciary' => $Lang_[2]), 'tm_lastupdate' =>$LsUp, 'tm_logo' =>$Logo));
+
+//author insert
+for($i = 0; $i <= $comp; i++){
+	$column_family -> insert($Name,'tm_author' => $Author[$i]);
+}
+
 $status ="Sucess to insert / update DB";
 return $status;
 

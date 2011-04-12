2819d95464dd16dc36cbb99df19952b9bed6b9b0
Gonalo Esteves <goncalo.e@gmail.com>
2011-04-12 21:32:42 +0100

    Actualização - corrigido projectos sem nome.

--- Programadores/scriptFinais/bing_launchpad.php
+++ Programadores/scriptFinais/bing_launchpad.php
@@ -7,14 +7,16 @@ while ($i <= 230) {
 	$bing_array = get_bing_launchpad(10, $i);
 	foreach($bing_array as $arg) {
 		list ($name_project, $owner, $language, $created_date, $logo) = $arg;
-		print_r("<b>Project:</b> ".$name_project."<br>");
-		print_r("<b>Source:</b> Launchpad<br>");
-		print_r("<b>Owner:</b> ".$owner."<br>");
-		foreach($arg[2] as $arg_lang) {
-			print_r("<b>Languages:</b> ".$arg_lang."<br>");
+		if (isset($name_project)) {
+			print_r("<b>Project:</b> ".$name_project."<br>");
+			print_r("<b>Source:</b> Launchpad<br>");
+			print_r("<b>Owner:</b> ".$owner."<br>");
+			foreach($arg[2] as $arg_lang) {
+				print_r("<b>Languages:</b> ".$arg_lang."<br>");
+			}
+			print_r("<b>Date Created:</b> ".$created_date."<br>");
+			print_r("<b>Logo:</b> <img src='".$logo."'><br><hr><br>");
 		}
-		print_r("<b>Date Created:</b> ".$created_date."<br>");
-		print_r("<b>Logo:</b> <img src='".$logo."'><br><hr><br>");
 	}
 	$i=$i+10;
 }
@@ -31,10 +33,13 @@ function get_bing_launchpad($lenght, $offset) {
 	$response = file_get_contents($request);
 	//descodifica a pagina
 	$bing_json = json_decode($response);
+//	echo "<pre>";
+//	print_r($bing_json);
+//	echo "</pre>";
 	$bing_array = array(); //cria o array
 	foreach($bing_json->SearchResponse->Web->Results as $arg) {
 		//retira o nome do utilizador atraves do URL
-		preg_match("/https:\/\/launchpad.net\/([A-Za-z0-9-_]*).*/", $arg->Url, $match);
+		preg_match("/https:\/\/launchpad.net\/([A-Za-z0-9-_~]*).*/", $arg->Url, $match);
 		if (isset($match[1])) {
 			//match[1] = utilizador
 			array_push($bing_array, get_project_launchpad($match[1])); //adiciona os projectos ao array

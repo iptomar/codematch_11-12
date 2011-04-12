44c357ea274ccd771be8b118b54208a3ae532367
Bruno Macao <bruno.macao@gmail.com>
2011-04-12 22:57:42 +0100

    update

--- Programadores/DBCola/DB_functions/db_insert.php
+++ Programadores/DBCola/DB_functions/db_insert.php
@@ -1,5 +1,6 @@
 <?php
-
+require_once('phpcassa/connection.php');
+require_once('phpcassa/columnfamily.php');
 //This function inserts/update data on DB and returns the function stats due to sucess or fail of the operation
 // Usage:
 // include 'db_insert.php'; -> on API search script file
@@ -7,8 +8,7 @@
 
 function insert_db($Name,$Fullname='',$Source,$Repo,$Author,$Lang_='',$LsUp,$Logo=''){
 try{
-require_once('phpcassa/connection.php');
-require_once('phpcassa/columnfamily.php');
+
 
 //make connection with database
 $conn = new Connection('tolmai');
@@ -17,6 +17,7 @@ $conn = new Connection('tolmai');
 $column_family= new ColumnFamily($conn , 'tm');
 
 $comp = count($Author);
+$cp = count($Lang);
 
 //insert column family rows
 //currently Database Proposed Schema
@@ -24,21 +25,20 @@ $comp = count($Author);
 //Single columns ->tm_fullname - fullname; tm_source - project url; tm_repository - repository url ;
 // tm_author [Array]- projects author; tm_language [Array] - project languages ; tm_lastupdate - projects last update; tm_logo - projects logo url
 //SuperColumn tm_language -> To decide how many entries will take, this function draft takes 3 entries ordered by project relevance
-$column_family->insert($Name,array('tm_fullname' =>$Fullname,'tm_source' =>$Source,'tm_repository' =>$Repo,'tm_author'=>$Author, 'tm_language' =>$Lang_, 'tm_lastupdate' =>$LsUp, 'tm_logo' =>$Logo));
+$column_family->insert($Name,array('tm_fullname' =>$Fullname,'tm_source' =>$Source,'tm_repository' =>$Repo,'tm_lastupdate' =>$LsUp, 'tm_logo' =>$Logo));
 
-
+//lang insert
+for ($l = 0; $i <=cp; $l++){
+	$column_family -> insert($Name,array('tm_language' => array($Lang_[$l] => $Lanf_[$l])));
+}
 
 //author insert
-//for($i = 0; $i <= $comp; $i++){
-//	$column_family -> insert($Name,array('tm_author' => array($Author[$i] => $Author[$i])));
-//}
-
-$status ="Sucess to insert / update DB";
-return $status;
+for($i = 0; $i <= $comp; $i++){
+	$column_family -> insert($Name,array('tm_author' => array($Author[$i] => $Author[$i])));
+}
 
 }catch(Exception $x){
-	$status = "Fail to insert / update DB" ;
-	return $status;
+	error_log($x);
 }
 }
 //Json Representation

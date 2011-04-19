621335b21d0c3cf8a50ac8cd3e9a16dd6a3e3486
aGonc <ag_gui99@hotmail.com>
2011-04-19 12:06:35 +0100

    correção de erros

--- Programadores/scriptFinais/yahoo_launchpad.php
+++ Programadores/scriptFinais/yahoo_launchpad.php
@@ -7,8 +7,8 @@ include "APIS/launchpad.php"; //include da api launchpad
 $i=0;
 while ($i <= 1) {
 	$yahoo_array = get_yahoo_launchpad(10, $i);
-	foreach($yahoo_array as $arg) {
-		list ($name_project, $title, $source, $owner, $language, $created_date, $logo) = $arg;
+	foreach($yahoo_array as $exe) {
+		list ($name_project, $title, $source, $owner, $language, $created_date, $logo) = $exe;
 		if (isset($name_project)) {
 			//insert_db($name_project, $title, $source, "Launchpad", $owner, $language, $created_date, $logo);
 			print_r("<b>Project:</b> ".$name_project."<br>");
@@ -16,14 +16,15 @@ while ($i <= 1) {
 			print_r("<b>Source:</b> ".$source."<br>");
 			print_r("<b>Repository:</b> Launchpad<br>");
 			print_r("<b>Owner:</b> ".$owner[0]."<br>");
-			foreach($language as $arg_lang) {
-				print_r("<b>Languages:</b> ".$arg_lang."<br>");
+			foreach($language as $exe_lang) {
+				print_r("<b>Languages:</b> ".$exe_lang."<br>");
 			}
 			print_r("<b>Date Created:</b> ".$created_date[0]."<br>");
 			print_r("<b>Date Updated:</b> ".$created_date[1]."<br>");
 			print_r("<b>Logo:</b> <img src='".$logo."'><br><hr><br>");;
 	}
 	$i=$i+1;
+}
 }
 
 
@@ -45,9 +46,9 @@ function get_yahoo_launchpad($lenght, $offset) {
 	//descodifica a pagina
 	$yahoo_json = json_decode($page);
 	$yahoo_array = array(); //cria o array
-	foreach($yahoo_json->ysearchresponse->resultset_web as $arg) {
+	foreach($yahoo_json->ysearchresponse->resultset_web as $projs) {
 		//retira o nome do utilizador atraves do URL
-		preg_match("/https:\/\/launchpad.net\/([A-Za-z0-9-_]*).*/", $arg->url, $match);
+		preg_match("/https:\/\/launchpad.net\/([A-Za-z0-9-_]*).*/", $projs->url, $match);
 		if (!empty($match[1])) {
 			//match[1] = utilizador
 			array_push($yahoo_array, get_project_launchpad($match[1])); //adiciona os projectos ao array

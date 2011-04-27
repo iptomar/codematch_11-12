4e47d9188593037f1576506bdb2e726b1e66cd69
Gonalo Esteves <goncalo.e@gmail.com>
2011-04-27 18:06:46 +0100

    Script Yahoo Github a Funcionar.

--- Programadores/scriptFinais/yahoo_github.php
+++ Programadores/scriptFinais/yahoo_github.php
@@ -4,36 +4,36 @@ include "db_insert.php";
 
 //o ciclo while e para ir mudando de pagina
 $i=0;
-while ($i <= 1) {
-	//$argv[1] - argumento na linha de comandos
-	$yahoo_array = get_yahoo_github(10, $i);
+while ($i <= 1000) {
+	$yahoo_array = get_yahoo_github(50, $i);
     foreach($yahoo_array as $arg) {
 		list ($name_project, $title, $source, $owner, $language, $created_date, $logo) = $arg;
 		if (isset($name_project)) {
-			//insert_db($name_project, $title, $source, "Github", $owner, $language, $created_date, $logo);
-			print_r("<b>Project:</b> ".$name_project."<br>");
-			print_r("<b>Title:</b> ".$title."<br>");
-			print_r("<b>Source:</b> ".$source."<br>");
-			print_r("<b>Repository:</b> Github<br>");
-			print_r("<b>Owner:</b> ".$owner[0]."<br>");
-			foreach($language as $arg_lang) {
-				print_r("<b>Languages:</b> ".$arg_lang."<br>");
-			}
-			print_r("<b>Date Created:</b> ".$created_date[0]."<br>");
-			print_r("<b>Date Updated:</b> ".$created_date[1]."<br>");
-			print_r("<b>Logo:</b> <img src='".$logo."'><br><hr><br>");
+			insert_db($name_project, $title, $source, "Github", $owner, $language, $created_date, $logo);
+//			print_r("<b>Project:</b> ".$name_project."<br>");
+//			print_r("<b>Title:</b> ".$title."<br>");
+//			print_r("<b>Source:</b> ".$source."<br>");
+//			print_r("<b>Repository:</b> Github<br>");
+//			print_r("<b>Owner:</b> ".$owner[0]."<br>");
+//			foreach($language as $arg_lang) {
+//				print_r("<b>Languages:</b> ".$arg_lang."<br>");
+//			}
+//			print_r("<b>Date Created:</b> ".$created_date[0]."<br>");
+//			print_r("<b>Date Updated:</b> ".$created_date[1]."<br>");
+//			print_r("<b>Logo:</b> <img src='".$logo."'><br><hr><br>");
  		}
-      $i=$i+1;
 	}
+	$i=$i+50;
 }
 print_r("Done Yahoo Github ".date("Y-m-d")."\n");
+
 
 // Parametros:
 //	- n/a
 // Retorna:
 //	- array() (retorna array com os dados do get_project_github)
 function get_yahoo_github($lenght, $offset) {
-	$thequery = urlencode('"http://github.com/"site:github.com');
+	$thequery = urlencode('"http://github.com/*/*"site:github.com');
 	$apikey = 'po6V4W7IkY2t6hn8Ab51nFT_HKtEocokU.E-';
 	$url = 'http://boss.yahooapis.com/ysearch/web/v1/'.$thequery.'?&format=json&count='.$lenght.'&appid='.$apikey.'&start='.$offset.'';
     $ch = @curl_init(); //inicia uma nova sessao
@@ -46,7 +46,7 @@ function get_yahoo_github($lenght, $offset) {
 	$yahoo_array = array(); //cria o array
 	foreach($yahoo_json->ysearchresponse->resultset_web as $arg) {
 		//retira o nome do projecto e utilizador atraves do URL
-		preg_match("/https:\/\/github.com\/([A-Za-z0-9]*)\/([A-Za-z0-9-_~]*).*/", $arg->url, $match);
+		preg_match("/github.com\/([A-Za-z0-9]*)\/([A-Za-z0-9-_~]*).*/", $arg->url, $match);
 		if ((isset($match[2])) && ($match[1]!="blog")) { //o nome do utilizador nao pode ser blog
 			//match[1] = utilizador, match[2] =  nome projecto
 			array_push($yahoo_array, get_project_github($match[1], $match[2])); //adiciona os projectos ao array

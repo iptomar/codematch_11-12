5fd5036773a8799996c7e4fc60809666a4cb4d16
carma <carmafernandes@gmail.com>
2011-05-14 18:12:58 +0100

    Correcao em test_get_last_updated.php

--- Programadores/DBCola/DB_functions - Versao Single Columns/ultimos_5_reps/test_get_date.php
+++ Programadores/DBCola/DB_functions - Versao Single Columns/ultimos_5_reps/test_get_date.php
@@ -1,30 +1,25 @@
 <?php
-
 require ("get_last_updated.php");
-
 $resultado = get_date("Github");
 
-//$res = array_slice($resultado, 0, 5);
-
 echo "Projectos mais acedidos do repositorio Github<br /><br /><br />";
-foreach ($resultado as $key=>$value) {
-//foreach ($res as $key=>$value) {
-//echo "$key.<br/>";
-foreach ($value as $iKey => $iValue) {
-if ($iKey == 'tm_date_c'){
-echo "Data de criacao:$iValue <br/>";
-}
-else
-{echo "Nome do projecto:$iValue <br/>";
-}
-//echo "$iValue <br/><br/>";
-}
+foreach ($resultado as $key=>$value) 
+{
+	//indices de array
+	foreach ($value as $iKey => $iValue)
+	{
+		if ($iKey == 'tm_date_l')
+		{
+			//coluna data de criacao
+			echo "Data de criacao:$iValue <br/>";
+		}
+		else
+		{
+			//coluna fullname
+			echo "Nome do projecto:$iValue <br/>";
+		}
+	}
 echo "------------------------------------------<br /><br />";
 }
-
-//print_r($resultado);
-//print_r("\n")
-
-
 
 ?>

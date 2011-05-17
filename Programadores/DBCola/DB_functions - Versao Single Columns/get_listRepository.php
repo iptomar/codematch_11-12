9e24f66d008b9a16f9727063b81a13de676d2f5d
Coquim <filipecoquim@gmail.com>
2011-05-17 16:45:10 +0100

    list Repository

---
+++ Programadores/DBCola/DB_functions - Versao Single Columns/get_listRepository.php
@@ -0,0 +1,28 @@
+<?php
+//lista todos os projectos na base de bados
+require_once('phpcassa/connection.php');
+require_once('phpcassa/columnfamily.php');
+
+
+	function get_listRepository($repo){
+$conn = new Connection('tolmai');
+	$column_family= new ColumnFamily($conn,'tm');
+	
+		$index_exp = CassandraUtil::create_index_expression('tm_repository',$repo);
+	$index_clause = CassandraUtil::create_index_clause(array($index_exp));
+	$rows = $column_family->get_indexed_slices($index_clause);
+	
+
+	
+
+	
+
+
+	foreach($rows as $key => $columns) {
+	
+		return ($columns);
+	
+	}
+	}
+	
+?>

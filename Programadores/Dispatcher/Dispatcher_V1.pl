4e7d045e96cbcc5405cebb99e5b90b612863194d
Semiao <estt8744@ipt.pt>
2011-05-25 12:24:09 +0100

    Dispatcher Improvements

--- Programadores/Dispatcher/Dispatcher_V1.pl
+++ Programadores/Dispatcher/Dispatcher_V1.pl
@@ -6,16 +6,23 @@
 use IPC::Open3;
 use threads;
 use threads::shared;
-use subs::parallel;
+
 
 #Variables
 
+open(LOG1,">/home/semiao/public_html/");
+
 #commands to use
 my $cmd1 = "php bing_github.php";
+
 my $cmd2 = "php bing_launchpad.php";
+
 my $cmd3 = "php bing_sourceforge.php";
+
 my $cmd4 = "php yahoo_github.php";
+
 my $cmd5 = "php yahoo_launchpad.php";
+
 my $cmd6 = "php yahoo_sourceforge.php";
 
 
@@ -24,37 +31,38 @@ menu();
 print "\n\n\n\n";
 
 #Start all threads
-$thr1 = threads->new(\&executa($cmd1)); 
+
+
+$thr1 = threads->new(sub{executa($cmd1)});
+
 #Starts $thread 1
-print "Thread 1 Started!\n Command: $cmd1 \n\n";
+print "Thread 1 Started!\n Command: $cmd1 \n";
 
-$thr2 = threads->new(\&executa($cmd2)); 
+$thr2 = threads->new(sub{executa($cmd2)}); 
 #Starts $thread 2
-print "Thread 2 Started!\n Command: $cmd2 \n\n";
+print "Thread 2 Started!\n Command: $cmd2 \n";
 
-$thr3 = threads->new(\&executa($cmd3)); 
+$thr3 = threads->new(sub{executa($cmd3)}); 
 #Starts $thread 3
-print "Thread 3 Started!\n Command: $cmd3 \n\n";
+print "Thread 3 Started!\n Command: $cmd3 \n";
 
-$thr4 = threads->new(\&executa($cmd4));
+$thr4 = threads->new(sub{executa($cmd4)});
 #Starts $thread 4
-print "Thread 4 Started!\n Command: $cmd4 \n\n";
+print "Thread 4 Started!\n Command: $cmd4 \n";
 
-$thr5 = threads->new(\&executa($cmd5));
+$thr5 = threads->new(sub{executa($cmd5)});
 #Starts $thread 5
-print "Thread 5 Started!\n Command: $cmd5 \n\n";
+print "Thread 5 Started!\n Command: $cmd5 \n";
 
-$thr6 = threads->new(\&executa($cmd6));
+$thr6 = threads->new(sub{executa($cmd6)});
 #Starts $thread 6
-print "Thread 6 Started!\n Command: $cmd6 \n\n";
-
-print "\n\nExiting...! \n\n Goodbye! \n\n";
+print "Thread 6 Started!\n Command: $cmd6 \n";
 
 #Functions
 
 #Call - execute a command - returns error code (0 = Fine process execution)
 
-sub executa : parallel
+sub executa 
 {	
 	#saves the command
 	my $comm = $_[0];
@@ -74,19 +82,25 @@ sub executa : parallel
 	#saves return value
 	my $ret = $?;
 	
+	#save termination time in log file
+	
+		
+	
 	#saves and shows script output
 	my $output = $wrt;
 	print "$wrt";
 	
 	#State returned
-	print "\n $ret returned - Good execution of process\n\n";
-
+	
+	if($ret == 0){
+	print "$ret returned - Good execution of process\n";
+	}
 	
 }
 
-#Menu Function - Draws initial menu
+#Menu Function - Print initial menu
 
-sub menu : parallel{
+sub menu {
 
 	#Save username
 	open FH, "whoami|" or die "Failed";
@@ -99,11 +113,7 @@ sub menu : parallel{
 	print "*               Version 1.0                  *\n";
 	print "**********************************************\n";
 	print "                                              \n";
-	print " 	          Welcome, $who[0]\n               ";
+	print " 	          Welcome, $who[0]\n             \n";
 	print "                                              \n";
 	print "**********************************************\n";
-	print "";
-	print "";
-	print "";
-
 }

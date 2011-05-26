66658e722ceab9fb4bbff5502dc46189cc5a8808
Bruno Macao <bruno.macao@gmail.com>
2011-05-26 01:47:43 +0100

    comments and query range update

--- Programadores/DBCola/NewSchema/insert_DB.php
+++ Programadores/DBCola/NewSchema/insert_DB.php
@@ -6,32 +6,44 @@ require_once('../phpcassa/columnfamily.php');
 include_once('../phpcassa/uuid.php');
 
 
+//this function inserts all data to all FOUR main databases
+/*INPUTS:
+proj_name - nome do projecto <string>
+source - url do projecto <string>
+repos - repositorio de origem <string>
+data_c - data criacao <date format>
+data_lst - data ultima actualizacao <date format>
+logo - logotipo url <string>
+lang - linguagens do projecto <array -string>
+author - autor/es do projecto <array -string>
+*/
 
 function insert($proj_name,$source,$repos,$data_c,$data_lst,$logo,$lang,$author){
 $conn = new Connection('DBCola');
 
+//select databases
 $column_detail= new ColumnFamily($conn,'projects');
 $column_lang= new ColumnFamily($conn,'language');
 $column_author= new ColumnFamily($conn,'author');
 $column_repos= new ColumnFamily($conn,'repos');
 
-//project details DB
+//insert data on project details DB
 $column_detail -> insert($proj_name, array('source' => $source, 'date_c' => $data_c, 'date_l' => $data_lst, 'logo' => $logo));
 
 
-//languages DB
+//insert data on languages DB
 $cp = count($lang);
 for ($l = 0; $l < $cp; $l++){
 	$column_lang -> insert($lang[$l],array(CassandraUtil::uuid1() => $proj_name));
 }
 
-//authors DB
+//insert data on authors DB
 $comp = count($author);
 for ($j = 0; $j < $cp; $j++){
 	$column_author -> insert($author[$j],array(CassandraUtil::uuid1() => $proj_name));
 }
 
-//repository DB
+//insert data on repository DB
 $column_repos ->insert($repos,array(CassandraUtil::uuid1() => $proj_name));
 
 

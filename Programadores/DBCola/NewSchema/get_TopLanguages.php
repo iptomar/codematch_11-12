28febbffcf2440753cd45e1274554682caea5072
Coquim <filipecoquim@gmail.com>
2011-05-26 05:27:04 +0100

    Top Languages geral

---
+++ Programadores/DBCola/NewSchema/get_TopLanguages.php
@@ -0,0 +1,39 @@
+<?php
+require_once('../phpcassa/connection.php');
+require_once('../phpcassa/columnfamily.php');
+
+include_once('../phpcassa/uuid.php');
+
+function top(){
+$conn = new Connection('DBCola');
+
+//selects language database
+$column_lang= new ColumnFamily($conn,'language');
+$arr_lang = array();
+$count =0;
+
+$rows = $column_lang->get_range($key_start='', $key_finish='');
+	
+	foreach ($rows as $key=>$value) 
+	{
+			$aux=$column_lang ->get_count($key);
+			$count=$count+$aux;			
+			//nome da linguagem
+			$lang = $key;
+			//linguagem->nº
+			$arr_lang[$lang] = $aux;
+		}
+		
+		//mete na primeira posição do array o numero total de linguagens
+		//$arr_lang['total'] = $count;
+		
+		//ordena o array
+		arsort($arr_lang);
+
+
+
+return($arr_lang);
+
+}
+
+?>

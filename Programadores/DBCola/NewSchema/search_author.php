83cc5823beedf29189e2f931df8da6d448a96632
Bruno Macao <bruno.macao@gmail.com>
2011-05-26 01:47:54 +0100

    comments and query range update

--- Programadores/DBCola/NewSchema/search_author.php
+++ Programadores/DBCola/NewSchema/search_author.php
@@ -4,13 +4,19 @@ require_once('../phpcassa/columnfamily.php');
 
 include_once('../phpcassa/uuid.php');
 
+//this function search and get all projects from a given author
+//input: repository <string>
 function search_author($authr){
 
 $conn = new Connection('DBCola');
 
+//selects author database
 $column_authr = new ColumnFamily($conn,'author');
 
-$pesquisa = $column_authr ->get($authr);
+//search author database
+$pesquisa = $column_authr ->get($authr,null,"","",false,100000,null,null);
+
+//return results
 return $pesquisa;
 }
 

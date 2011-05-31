6b883b6827343605490b04c9748a61954bec6c0f
jccferreira <jccostaferreira@gmail.com>
2011-05-31 22:40:24 +0100

    04
    Signed-off-by: jccferreira <jccostaferreira@gmail.com>

---
+++ Design/antigo/site_ferreira_04/DB_functions/get_RepoStats.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('phpcassa/connection.php');
+require_once('phpcassa/columnfamily.php');
+
+//this function searches the repository content
+//returns the projects count
+//receives the repo name as argument
+function get_RepoStats($repo){
+	
+$conn = new Connection('tolmai');
+
+$column_family= new ColumnFamily($conn,'tm');	
+	
+//------------------- Search repository ----------------
+$index_exp = CassandraUtil::create_index_expression('tm_repository',$repo);
+$index_clause = CassandraUtil::create_index_clause(array($index_exp));
+$rows = $column_family->get_indexed_slices($index_clause);
+
+//project counter
+$count =0;
+foreach($rows as $key => $columns) {
+    //count matches
+	$count++;
+   
+}
+
+//return hit count
+echo $count;
+
+
+}
+
+?>

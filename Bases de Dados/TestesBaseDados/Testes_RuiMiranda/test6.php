7d3d36747d42b12c3e8e3e1a010ff785cec28412
ruimiranda <rmfmiranda@gmail.com>
2011-06-01 18:15:31 +0100

    adicao de testes a base de dados

---
+++ Bases de Dados/TestesBaseDados/Testes_RuiMiranda/test6.php
@@ -0,0 +1,32 @@
+<?php
+//Vai buscar todos os projectos da tabela tm e insere as linguagens de cada de projecto na tabela teste_logs
+
+require_once('phpcassa/connection.php');
+require_once('phpcassa/columnfamily.php');
+
+$conn = new Connection('tolmai');
+
+$column_family = new ColumnFamily($conn, 'tm');
+$logs = new ColumnFamily($conn, 'teste_logs');
+
+$numproj = 0;
+
+
+		$index = $column_family->get_range($key_start='mod_psgi', $key_finish='Epixa',1000000);
+
+		foreach($index as $key => $result) {
+			$perc_rand1=rand(0,100);
+			$perc_rand2=rand(0,100);
+			$perc_rand3=rand(0,100);
+			$perc_rand4=rand(0,100);
+			$logs->insert($key, array('language1' => $result['tm_language_P'], 'plang1' => $perc_rand1, 'language2' => $result['tm_language_S'], 'plang2' => $perc_rand2, 'language3' => $result['tm_language_S'], 'plang3' => $perc_rand3, 'language4' => $result['tm_language_T'], 'plang4' => $perc_rand4));
+			
+			$numproj++;
+		}
+		
+	
+print_r("<br>");
+
+echo("<p>Numero de projectos inseridos: $numproj\n\n</p>");
+
+?>

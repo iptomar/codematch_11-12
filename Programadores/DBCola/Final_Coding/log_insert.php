ee2f74ef667b2906a1bfd2312099234332580677
Coquim <filipecoquim@gmail.com>
2011-06-04 01:31:06 +0100

    update

---
+++ Programadores/DBCola/Final_Coding/log_insert.php
@@ -0,0 +1,78 @@
+<?php
+require_once('phpcassa/connection.php');
+require_once('phpcassa/columnfamily.php');
+//This function inserts/update data on DB and returns the function stats due to sucess or fail of the operation
+// Usage:
+// require ("log_insert.php"); -> on API search script file
+// db_insert(<arguments>); 9 arguments accepted - if some are inexistent use Zero value to maintain DB structure
+
+
+function insert_log($name,$nameproj,$lang, $plang, $total){
+   $lan_array = array_map("strtoupper", $lang);
+	try{
+		//make connection with database
+		$conn = new Connection('DBCola');
+
+		//select column family
+		$column_family= new ColumnFamily($conn , 'logs');
+
+		
+		
+				//single insert
+		$column_family-> insert($name,array("nameproj" => $nameproj));
+		
+			if(isset($lang[0])){ //1º lingua no array
+			$column_family-> insert($name,array("language1" =>$lan_array[0])); 
+		} else {
+			$column_family-> insert($name,array("language1" => "N/A"));
+		}	
+		if(isset($lang[1])){ //2º lingua no array
+			$column_family-> insert($name,array("language2" => $lan_array[1])); 
+		} else {
+			$column_family-> insert($name,array("language2" => "N/A"));
+		}			
+		if(isset($lang[2])){ //3º lingua no array
+			$column_family-> insert($name,array("language3" => $lan_array[2])); 
+		} else {
+			$column_family-> insert($name,array("language3" => "N/A"));
+		}
+		if(isset($lang[3])){ //4º lingua no array
+			$column_family-> insert($name,array("language4" => $lan_array[3])); 
+		} else {
+			$column_family-> insert($name,array("language4" => "N/A"));
+		}
+		
+
+		
+		if(isset($plang[0])){ // % da 1º lingua no array
+			$column_family-> insert($name,array("plang1" =>$plang[0])); 
+		} else {
+			$column_family-> insert($name,array("plang1" => "0"));
+		}	
+		if(isset($plang[1])){ //% da2º lingua no array
+			$column_family-> insert($name,array("plang2" => $plang[1])); 
+		} else {
+			$column_family-> insert($name,array("plang2" => "0"));
+		}			
+		if(isset($plang[2])){ //% da3º lingua no array
+			$column_family-> insert($name,array("plang3" => $plang[2])); 
+		} else {
+			$column_family-> insert($name,array("plang3" => "0"));
+		}
+		if(isset($plang[3])){ //% da4º lingua no array
+			$column_family-> insert($name,array("plang4" => $plang[3])); 
+		} else {
+			$column_family-> insert($name,array("plang4" => "0"));
+		}
+
+		if(isset($total)){ //total de ficheiros de cada projecto
+			$column_family-> insert($name,array("tfiles" => $total)); 
+		} else {
+			$column_family-> insert($name,array("tfiles" => "0"));
+		}
+	}
+	catch(Exception $x){
+		error_log($x);
+	}
+}
+?>
